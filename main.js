f208460338b102d9e57e648a4919f82a7de3025a
hasanovtemur05 <hasanovtemur86@gmail.com>
2024-08-18 14:10:33 +0500

    added about

--- main.js
+++ main.js
@@ -2,16 +2,22 @@ const add_task = document.getElementById("add")
 const save = document.getElementById("save_modal")
 const result = document.getElementById("result")
 const delete_modal = document.getElementById("delete_modal")
+const pagination = document.getElementById("pagination")
+
+let current_page = 1
+let product_per_page = 2
+
 let form = {}
 let product = []
 let baseUrl = "http://localhost:3000/avto"
+
 document.addEventListener('DOMContentLoaded', function(event){
     event.preventDefault()
     const modal = document.getElementById('modal');
     add_task.addEventListener('click', () => {
         modal.style.display = 'block';
     });
-     save.addEventListener("click", saveProduct)
+    save.addEventListener("click", saveProduct)
     window.addEventListener('click', (event) => {
         if (event.target === modal) {
             modal.style.display = 'none';
@@ -24,21 +30,26 @@ delete_modal.addEventListener("click", function(){
     modal.style.display = "none"
 })
 
- async function saveProduct(){
-    try{
-        let response = await fetch(`${baseUrl}`,{
-        method: "POST",
-        headers:{
-            "Content-Type" : "application/json"
-        },
-        body: JSON.stringify(form)
-
-    })
-   
-    
-    }catch(error){
-        console.log(error);
+async function saveProduct() {
+    try {
+        let method = form.id ? "PUT" : "POST";
+        let url = form.id ? `${baseUrl}/${form.id}` : baseUrl;
         
+        let response = await fetch(url, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(form)
+        });
+
+        if (response.ok) {
+            form = {};
+            document.getElementById('modal').style.display = 'none';
+            getProducts();
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
 
@@ -47,21 +58,23 @@ async function getProducts(){
         const response = await fetch(`${baseUrl}`)
         product = await response.json()
         displayProduct()
+        paginationProduct()
     }catch(error){
         console.log(error);
-        
     }
 }
 
 function handleChange(event){
     const {name, value} = event.target
     form = {...form, [name] : value}
-
 }
 
 function displayProduct(){
     result.innerHTML = ""
-    product.forEach((item) => {
+    let start_index = (current_page - 1) * product_per_page
+    let end_index = start_index + product_per_page
+    let pagination_product = product.slice(start_index, end_index)
+    pagination_product.forEach((item) => {
         result.innerHTML += `
         <div id="hero">
             <h1 class="title_hero">${item.name}</h1>
@@ -73,15 +86,28 @@ function displayProduct(){
             <p class="desc">Condition: <span class="item">${item.condition}</span></p>
             <p class="desc">Price: <span class="item">${item.price}</span></p>
             <div class="btns">
-                <button class="edit_hero">edit</button>
-                <button class="delete_hero" onclick="deleteProduct("${item.id}")">delete</button>
+                <button class="edit_hero" onclick="editProduct('${item.id}')">edit</button>
+                <button class="delete_hero" onclick="deleteProduct('${item.id}')">delete</button>
             </div>
         </div>
         `
-        
-     
     })
-    
+}
+
+function paginationProduct(){
+    pagination.innerHTML = ""
+    let page_count = Math.ceil(product.length / product_per_page)
+    for(let i = 1; i <= page_count; i++){
+        let btn = document.createElement("button")
+        btn.innerText = i
+        if (i === current_page) btn.classList.add("active")
+        btn.addEventListener('click', function() {
+            current_page = i
+            displayProduct()
+            paginationProduct()
+        });
+        pagination.appendChild(btn)
+    }
 }
 
 async function deleteProduct(id){
@@ -89,8 +115,34 @@ async function deleteProduct(id){
         await fetch(`${baseUrl}/${id}`,{
             method: "DELETE"
         })
+        getProducts(); 
     }catch(error){
         console.log(error);
-        
     }
 }
+
+async function editProduct(id) {
+    let obj = product.find((item) => item.id == id);
+    
+    if (obj) {
+        form = { ...obj }; 
+        document.querySelector("input[name='name']").value = form.name
+        document.querySelector("input[name='color']").value = form.color
+        document.querySelector("input[name='year']").value = form.year
+        document.querySelector("input[name='brand']").value = form.brand
+        document.querySelector("input[name='fuel']").value = form.fuel
+        document.querySelector("input[name='horsepower']").value = form.horsepower
+        document.querySelector("input[name='transmission']").value = form.transmission
+        document.querySelector("input[name='condition']").value = form.condition
+        document.querySelector("input[name='price']").value = form.price
+    
+        modal.style.display = 'block'; 
+    }
+}
+
+    
+   
+   
+
+
+
